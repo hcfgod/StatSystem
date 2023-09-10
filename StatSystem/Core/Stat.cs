804184c14894e72cf945c9a941a99f75296f3730
hcfgod <80559377+hcfgod@users.noreply.github.com>
2023-09-09 21:27:27 -0600

    Support for stat modifiers

---
+++ StatSystem/Core/Stat.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// The Stat class represents an individual statistic.
+public class Stat
+{
+	// Event triggered when the stat value changes.
+	public event Action<float> OnValueChanged;
+
+	public string Name { get; }
+	
+	public float Value
+	{
+		get => _value;
+		
+		private set
+		{
+			_value = value;
+			OnValueChanged?.Invoke(_value);
+		}
+	}
+	
+	private float _value;
+	
+	public float MaxValue { get; }
+
+	public Coroutine CurrentCoroutine { get; set; }
+	
+	private List<StatModifier> statModifiers = new List<StatModifier>();
+    
+	// Constructor to initialize a new Stat object.
+	public Stat(string name, float initialValue, float maxValue)
+	{
+		if (string.IsNullOrEmpty(name))
+		{
+			throw new ArgumentException("Name cannot be null or empty.");
+		}
+		if (initialValue < 0 || maxValue < 0)
+		{
+			throw new ArgumentException("Initial and max values must be non-negative.");
+		}
+		if (initialValue > maxValue)
+		{
+			throw new ArgumentException("Initial value cannot be greater than max value.");
+		}
+
+		Name = name;
+		Value = initialValue;
+		MaxValue = maxValue;
+	}
+
+    // Increases the stat value by a given amount.
+	public void IncreaseStatValue(float amount)
+	{
+		if (amount < 0)
+		{
+			throw new ArgumentException("Amount must be non-negative.");
+		}
+		
+		Value = Mathf.Min(Value + amount, MaxValue);
+	}
+
+	// Decreases the stat value by a given amount.
+	public void DecreaseStatValue(float amount)
+	{
+		if (amount < 0)
+		{
+			throw new ArgumentException("Amount must be non-negative.");
+		}
+		
+		Value = Mathf.Max(Value - amount, 0);
+	}
+	
+	 public void AddModifier(StatModifier modifier)
+	{
+		statModifiers.Add(modifier);
+		RecalculateValue();
+	}
+
+	public void RemoveModifier(StatModifier modifier)
+	{
+		statModifiers.Remove(modifier);
+		RecalculateValue();
+	}
+	
+	private void RecalculateValue()
+	{
+		float finalValue = Value;
+
+		// Apply additive modifiers
+		foreach (var mod in statModifiers.Where(m => m.Type == StatModifier.ModifierType.Additive))
+		{
+			finalValue += mod.Value;
+		}
+
+		// Apply multiplicative modifiers
+		foreach (var mod in statModifiers.Where(m => m.Type == StatModifier.ModifierType.Multiplicative))
+		{
+			finalValue *= mod.Value;
+		}
+
+		Value = finalValue;
+		OnValueChanged?.Invoke(Value);
+	}
+	
+	public List<StatModifier> GetStatModifierList()
+	{
+		return statModifiers;
+	}
+}
---
+++ StatSystem/Core/StatManager.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+// StatManager is responsible for managing multiple Stat objects.
+public class StatManager : MonoBehaviour, IDisposable
+{
+	// List of StatData objects.
+	public List<StatData> statDataList; // Assign in Unity Editor
+
+	private StatSystem statSystem;
+
+	void Awake()
+	{
+		statSystem = new StatSystem();
+		InitializeStats();
+	}
+
+	private void InitializeStats()
+	{
+		foreach (var data in statDataList)
+		{
+			AddStat(data);
+		}
+	}
+
+	// Centralized way of adding a new Stat to the system.
+	public void AddStat(StatData statData)
+	{
+		if (statData == null)
+		{
+			throw new ArgumentNullException("StatData cannot be null.");
+		}
+
+		Stat newStat = new Stat(name, statData.initialValue, statData.maxValue);
+		newStat.OnValueChanged += HandleStatValueChanged;
+		statSystem.AddStat(statData, newStat);
+	}
+
+	// Retrieves a Stat based on StatData.
+	public Stat GetStat(StatData statData)
+	{
+		if (statData == null)
+		{
+			throw new ArgumentNullException("StatData cannot be null.");
+		}
+
+		return statSystem.GetStat(statData);
+	}
+
+	public void IncreaseStatValue(StatData statData, float amount)
+	{
+		if (amount < 0)
+		{
+			throw new ArgumentException("Amount must be non-negative.");
+		}
+		
+		statSystem.IncreaseStatValue(statData, amount);
+	}
+
+	public void DecreaseStatValue(StatData statData, float amount)
+	{
+		if (amount < 0)
+		{
+			throw new ArgumentException("Amount must be non-negative.");
+		}
+		
+		statSystem.DecreaseStatValue(statData, amount);
+	}
+	
+	public void RegenerateStatValueOverTime(StatData statData, float delay, float regenAmount)
+	{
+		Stat stat = GetStat(statData);
+		
+		if (stat == null)
+		{
+			throw new InvalidOperationException("Stat not found.");
+		}
+		if (stat.CurrentCoroutine != null)
+		{
+			throw new InvalidOperationException("A coroutine is already running for this stat.");
+		}
+
+		stat.CurrentCoroutine = StartCoroutine(statSystem.RegenerateOverTime(this, stat, delay, regenAmount));
+	}
+
+	public void DepleteStatValueOverTime(StatData statData, float delay, float decrementAmount)
+	{
+		Stat stat = GetStat(statData);
+		
+		if (stat == null)
+		{
+			throw new InvalidOperationException("Stat not found.");
+		}
+		if (stat.CurrentCoroutine != null)
+		{
+			throw new InvalidOperationException("A coroutine is already running for this stat.");
+		}
+    
+		stat.CurrentCoroutine = StartCoroutine(statSystem.DepleteOverTime(this, stat, delay, decrementAmount));
+	}
+	
+	public void AddPermantantModifier(Stat stat, StatModifier modifier)
+	{
+		stat.AddModifier(modifier);
+	}
+	
+	public void RemovePermantantModifier(Stat stat, StatModifier modifier)
+	{
+		stat.RemoveModifier(modifier);
+	}
+	
+	public void AddTemporaryModifier(Stat stat, StatModifier modifier)
+	{
+		stat.AddModifier(modifier);
+		StartCoroutine(HandleModifierDuration(stat, modifier));
+	}
+	
+	public IEnumerator HandleModifierDuration(Stat stat, StatModifier modifier)
+	{
+		yield return new WaitForSeconds(modifier.Duration);
+		stat.RemoveModifier(modifier);
+	}
+	
+	public void StopStatCoroutine(StatData statData)
+	{
+		Stat stat = GetStat(statData);
+		if (stat == null) return;
+		
+		if(stat.CurrentCoroutine == null) return;
+			
+		StopCoroutine(stat.CurrentCoroutine);
+		stat.CurrentCoroutine = null;
+	}
+	
+	private void HandleStatValueChanged(float newValue)
+	{
+		Debug.Log("Stat changed. New value: " + newValue);
+	}
+	
+	
+	public void Dispose()
+	{
+		foreach(Stat stat in statSystem.GetStatDictionary().Values)
+		{
+			stat.OnValueChanged -= HandleStatValueChanged;
+		}
+	}
+}
