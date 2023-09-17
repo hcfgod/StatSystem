d76fa1a383cf000594ac0bd300c2d7e32aec8698
hcfgod <80559377+hcfgod@users.noreply.github.com>
2023-09-17 17:12:32 -0600

    Major Bug Fix! more Examples

---
+++ Stat System/Examples/Scripts/SpeedAgilityInteraction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StatSystem
+{
+	public class SpeedAgilityInteraction : IStatInteraction
+	{
+		public void ApplyInteraction(Stat speed, Stat agility)
+		{
+			// Custom logic to interact speed and agility
+			if (agility.Value > 50)
+			{
+				speed.IncreaseStatValue(2);
+			}
+			else
+			{
+				speed.DecreaseStatValue(1);
+			}
+		}
+	}
+}
